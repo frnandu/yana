bfd6f7fa3e260141437ce01899f47fcfca0dbe20
Fmar <_@fmar.link>
2023-09-25 16:15:13 +0200

    Revert "SwiftAwesomeNotificationsPlugin"
    This reverts commit 985cc6d0

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,6 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
-        // This function registers the desired plugins to be used within a notification background action
-      return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
