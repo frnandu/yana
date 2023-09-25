985cc6d0958b02187a837836edfdd291ca4e62f6
Fmar <_@fmar.link>
2023-09-25 14:05:49 +0200

    SwiftAwesomeNotificationsPlugin

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,6 +8,13 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+        // This function registers the desired plugins to be used within a notification background action
+      SwiftAwesomeNotificationsPlugin.setPluginRegistrantCallback { registry in
+          SwiftAwesomeNotificationsPlugin.register(
+            with: registry.registrar(forPlugin: "io.flutter.plugins.awesomenotifications.AwesomeNotificationsPlugin")!)
+          FLTSharedPreferencesPlugin.register(
+            with: registry.registrar(forPlugin: "io.flutter.plugins.sharedpreferences.SharedPreferencesPlugin")!)
+      }
+      return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
